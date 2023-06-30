a3d7b8cf570200b82cf2448386db52bf07ad4599
Mustafa Babadagi <fathermountain@gmail.com>
2023-06-30 10:36:09 -0400

    second version

--- src/test/java/pages/EnterAccountPage.java
+++ src/test/java/pages/EnterAccountPage.java
@@ -7,18 +7,46 @@ import utilities.Driver;
 
 public class EnterAccountPage {
 
-    public void EnterAccountPage(){
-
+    public EnterAccountPage() {
         PageFactory.initElements(Driver.getDriver(),this);
-
     }
 
+    @FindBy(xpath ="//*[text()='Enter Account Information']")
+    public WebElement enterAccountText;
 
-    @FindBy(xpath ="//b[text()='ENTER ACCOUNT INFORMATION']")
-     public WebElement enterAccountText;
 
-    @FindBy(id="id_gender1")
+    @FindBy(css="#id_gender1")
     public WebElement maleRadioButton;
+
+
+    @FindBy(xpath="//*[@data-qa='password']")
+    public WebElement UserPassword;
+
+    @FindBy(id="days")
+    public WebElement day;
+
+    @FindBy(id="months")
+    public WebElement month;
+
+    @FindBy(id="years")
+    public WebElement year;
+
+    @FindBy(id="uniform-newsletter")
+    public WebElement newsLetterCheckbox;
+
+    @FindBy(id="first_name")
+    public WebElement firstName;
+
+    @FindBy(id="last_name")
+    public WebElement lastName;
+
+    @FindBy(id="company")
+    public WebElement companyName;
+
+    @FindBy(id="address1")
+    public WebElement address1;
+
+
 
 
 
--- src/test/java/stepdefinitions/RegisterStepDefinitions.java
+++ src/test/java/stepdefinitions/RegisterStepDefinitions.java
@@ -1,13 +1,21 @@
 package stepdefinitions;
 
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import pages.AutomationMainPage;
 import pages.EnterAccountPage;
 import pages.SignUpLoginPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.JSUtils;
+import utilities.ReusableMethods;
+
+import java.sql.SQLOutput;
 
 public class RegisterStepDefinitions {
 
@@ -17,6 +25,8 @@ public class RegisterStepDefinitions {
      SignUpLoginPage signUpLoginPage=new SignUpLoginPage();
 
      EnterAccountPage enterAccountPage=new EnterAccountPage();
+
+     Faker faker=new Faker();
 
     @Given("User is on automation page")
     public void user_is_on_automation_page() {
@@ -39,8 +49,10 @@ public class RegisterStepDefinitions {
     }
     @Then("User enters name and email address")
     public void user_enters_name_and_email_address() {
-        signUpLoginPage.nameInputBox.sendKeys("Robert De Niro");
-        signUpLoginPage.emailInputBox.sendKeys("robertdeniro@gmail.com");
+        Faker faker1 =new Faker();
+        signUpLoginPage.nameInputBox.sendKeys(faker1.name().name());
+        signUpLoginPage.emailInputBox.sendKeys(faker1.internet().emailAddress());
+
     }
     @Then("User clicks {string} button")
     public void user_clicks_button(String string) {
@@ -50,9 +62,51 @@ public class RegisterStepDefinitions {
 
     @Then("User verifies that {string} is visible")
     public void user_verifies_that_is_visible(String string) {
+         ReusableMethods.waitFor(5);
+          String a=enterAccountPage.enterAccountText.getText();
+         System.out.println(a);
+          enterAccountPage.enterAccountText.isDisplayed();
+          ReusableMethods.waitForVisibility(enterAccountPage.enterAccountText,15).isDisplayed();
 
-       Assert.assertTrue(enterAccountPage.enterAccountText.isDisplayed());
+  //      ReusableMethods.waitForVisibility(enterAccountPage.enterAccountText,15).isDisplayed();
+//        WebElement enterAccount = enterAccountPage.enterAccountText; //Driver.getDriver().findElement(By.xpath("//*[text()='Enter Account Information']"));
+//        String expectedUser1 = "ENTER ACCOUNT INFORMATION";
+//        String actualUser1 = enterAccount.getText();
+//        Assert.assertEquals(expectedUser1, actualUser1);
+
+
+
     }
+    @Then("User fills details: Title, Name, Email, Password, Date of birth")
+    public void user_fills_details_title_name_email_password_date_of_birth() {
+       ReusableMethods.waitFor(3);
 
 
+//        JSUtils.clickElementByJS(enterAccountPage.maleRadioButton);
+//        ReusableMethods.waitFor(3);
+       enterAccountPage.maleRadioButton.click();
+
+
+        Assert.assertTrue(( enterAccountPage.maleRadioButton.isSelected()));//Verifing the red button is selected
+
+     enterAccountPage.UserPassword.sendKeys(faker.internet().password());
+        Select select1=new Select(enterAccountPage.day);
+        select1.selectByIndex(23);
+        Select select2=new Select(enterAccountPage.month);
+        select2.selectByIndex(7);
+        Select select3=new Select(enterAccountPage.year);
+        select3.selectByIndex(2);
+
+        enterAccountPage.firstName.sendKeys(faker.name().firstName());
+        enterAccountPage.lastName.sendKeys(faker.name().lastName());
+        enterAccountPage.companyName.sendKeys(faker.company().name());
+        enterAccountPage.address1.sendKeys(faker.address().fullAddress());
+
+
+
+
+
+
+
+    }
 }
